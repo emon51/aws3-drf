b03b8b30d9d676871c38ff82cc516b8a79969078
emon51 <emanur11235@gmail.com>
2026-04-13 10:57:58 +0600

    # Info
    - Initial project setup

---
+++ files/views.py
@@ -0,0 +1,13 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+# Create your views here.
+
+@api_view(["GET"])
+def home(request):
+    return Response(
+        {
+            "message": "Hello, World"
+        }
+    )
+
